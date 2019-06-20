d98829b7e4796f46b01b8e16d5d1fca63882666a
Khachornchit Songsaen <khachornchit.songsaen@thomsonreuters.com>
2019-06-21 00:43:46 +0700

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AlertModule, ButtonsModule, TabsModule } from 'ngx-bootstrap';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AlertModule.forRoot(),
+    ButtonsModule.forRoot(),
+    TabsModule.forRoot(),
     BrowserModule,
     AppRoutingModule
   ],
